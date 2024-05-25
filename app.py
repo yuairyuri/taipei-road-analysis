8ba98f140553ea58e2f48ac081ddbea710a94a2f
yuairyuri <yuairred0951@gmail.com>
2024-05-25 13:24:22 +0800

    feat: road cluster map
    1. Share data between callbacks
    2. Replace cluster centers on radar charts with cluster medians
    3. Show clustering results on map
    4. Resize map

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from dash import Dash, dcc, html, Input, Output, callback
+from io import StringIO
+import time
 import json
 import pandas as pd
 import geopandas as gpd
@@ -19,13 +21,14 @@ app.layout = html.Div([
                 dcc.Dropdown(id='cluster', multi=True)
             ]),
             html.Div(className='chart', children=[
-                dcc.Graph(figure={}, id='polar-chart')
+                dcc.Graph(figure={}, id='radar-chart')
             ])
         ]),
         html.Div(className='right', children=[
             dcc.Graph(figure={}, id='road')
         ])
-    ])
+    ]),
+    dcc.Store(id='label')
 ])
 
 @app.callback(
@@ -37,21 +40,31 @@ def select_cluster(k_value):
     return options
 
 @app.callback(
-    Output('polar-chart', 'figure'),
+    Output('label', 'data'),
     Input('k-range', 'value')
 )
 def clustering(k_value):
     df = pd.read_csv("./data/major_road_data.csv").set_index('OBJECTID')
     X = df.iloc[:, 1:]
     kmeans = KMeans(n_clusters=k_value, random_state=31).fit(X)
+    df['cluster_id'] = kmeans.labels_
+    return df.reset_index().to_json(orient='index')
 
+@app.callback(
+    Output('radar-chart', 'figure'),
+    Input('label', 'data')
+)
+def show_features(data):
     fig = go.Figure()
-    for i in range(kmeans.n_clusters):
+    time.sleep(1)
+    labeled_df = pd.read_json(StringIO(data), orient='index').set_index('OBJECTID')
+    X = labeled_df.iloc[:, 1:-1]
+    for i in labeled_df['cluster_id'].unique():
         fig.add_trace(go.Scatterpolar(
-            r=kmeans.cluster_centers_[i],
+            r=labeled_df.groupby('cluster_id').median().iloc[i, 1:],
             theta=X.columns,
             fill='toself',
-            name=i
+            name=str(i)
         ))
     fig.update_layout(
         polar=dict(
@@ -65,19 +78,26 @@ def clustering(k_value):
 
 @app.callback(
     Output('road', 'figure'),
+    Input('label', 'data'),
     Input('cluster', 'value')
 )
-def draw_road(selected_cluster):
-    road_gdf = gpd.read_file("./data/roads.geojson").to_crs(4326).set_index('OBJECTID')
+def draw_road(data, selected_cluster):
+    labeled_df = pd.read_json(StringIO(data), orient='index')
+    road_gdf = gpd.read_file("./data/roads.geojson").to_crs(4326)
+    road_gdf = road_gdf.merge(labeled_df, on='OBJECTID').set_index('OBJECTID')
     fig = go.Figure(go.Choroplethmapbox(
         geojson=json.loads(road_gdf.geometry.to_json()),
         locations=road_gdf.index,
-        z=road_gdf['RoadWidth']
+        z=road_gdf['cluster_id']
     ))
     fig.update_layout(
         mapbox_style="carto-positron",
         mapbox_zoom=12,
         mapbox_center={"lat": 25.0375, "lon": 121.5625}
+    )
+    fig.update_layout(
+        height=750,
+        margin={'r': 16, 't': 16, 'l': 16, 'b': 16}
     )
     return fig
 
